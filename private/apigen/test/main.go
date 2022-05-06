654f1bf8b5495db8062f04c76ae871e0357af9d6
Moby von Briesen <mobyvb@gmail.com>
2022-05-06 14:49:57 -0400

    private/apigen: Typescript type generation
    Change-Id: Ic59737465cb9ec25f795ec2070e8f1ae5eee5401

---
+++ private/apigen/test/main.go
@@ -0,0 +1,85 @@
+// Copyright (C) 2022 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package main
+
+//go:generate go run ./
+
+import (
+	"storj.io/storj/private/apigen"
+)
+
+type Project struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	// definition for REST API
+	a := &apigen.API{
+		Version:     "v0",
+		Description: "",
+		PackageName: "test",
+	}
+
+	{
+		g := a.Group("ProjectManagement", "projects")
+
+		g.Get("/", &apigen.Endpoint{
+			Name:        "Get Projects",
+			Description: "Gets all projects user has",
+			MethodName:  "GenGetUsersProjects",
+			Response:    Project{},
+		})
+
+		/*
+			g.Get("/bucket-rollup", &apigen.Endpoint{
+				Name:        "Get Project's Single Bucket Usage",
+				Description: "Gets project's single bucket usage by bucket ID",
+				MethodName:  "GenGetSingleBucketUsageRollup",
+				Response:    &accounting.BucketUsageRollup{},
+				Params: []apigen.Param{
+					apigen.NewParam("projectID", uuid.UUID{}),
+					apigen.NewParam("bucket", ""),
+					apigen.NewParam("since", time.Time{}),
+					apigen.NewParam("before", time.Time{}),
+				},
+			})
+
+			g.Get("/bucket-rollups", &apigen.Endpoint{
+				Name:        "Get Project's All Buckets Usage",
+				Description: "Gets project's all buckets usage",
+				MethodName:  "GenGetBucketUsageRollups",
+				Response:    []accounting.BucketUsageRollup{},
+				Params: []apigen.Param{
+					apigen.NewParam("projectID", uuid.UUID{}),
+					apigen.NewParam("since", time.Time{}),
+					apigen.NewParam("before", time.Time{}),
+				},
+			})
+
+			g.Post("/create", &apigen.Endpoint{
+				Name:        "Create new Project",
+				Description: "Creates new Project with given info",
+				MethodName:  "GenCreateProject",
+				Response:    &console.Project{},
+				Params: []apigen.Param{
+					apigen.NewParam("projectInfo", console.ProjectInfo{}),
+				},
+			})
+
+			g.Patch("/update/{id}", &apigen.Endpoint{
+				Name:        "Update Project",
+				Description: "Updates project with given info",
+				MethodName:  "GenUpdateProject",
+				Response:    &console.Project{},
+				Params: []apigen.Param{
+					apigen.NewParam("id", uuid.UUID{}),
+					apigen.NewParam("projectInfo", console.ProjectInfo{}),
+				},
+			})
+		*/
+	}
+
+	// a.MustWriteGo("satellite/console/consoleweb/consoleapi/api.gen.go")
+	a.MustWriteTS("testts.gen.go")
+}
